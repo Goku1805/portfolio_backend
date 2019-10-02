005393b819e32ba8d7c73dc867f2812a93ad7e16
Gokul <gokulrajan18.05@gmail.com>
2019-10-02 16:39:30 +0530

    updation

--- db.js
+++ db.js
@@ -25,9 +25,20 @@ app.use(
   )
 );
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("src/build"));
+  app.use(express.static("../../resume/src/components/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "src", "build", "index.html"));
+    res.sendFile(
+      path.join(
+        __dirname,
+        "..",
+        "..",
+        "resume",
+        "src",
+        "components",
+        "build",
+        "index.html"
+      )
+    );
   });
 }
 app.post("/post-feedback", function(req, res) {
